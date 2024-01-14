49ae2a2a616a4bd478e6cf9b80176e1db535dcc0
anuksalgado <118955816+anuksalgado@users.noreply.github.com>
2024-01-14 18:41:32 +0800

    Changes

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // Include HTML files in the src directory
+    "./public/**/*.js",   // Include JavaScript files in the src directory
+    "./public/**/*.css",  // Include CSS files in the src directory
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        playfair: ['PlayFair Display', 'serif'],
+        playfairExtraBold: ['PlayFair Display', 'serif', 'ExtraBold']
+      },
+      colors: {
+        cyan: {
+          50: '#e0f7fa',
+          100: '#b2ebf2',
+          200: '#80deea',
+          300: '#4dd0e1',
+          400: '#26c6da',
+          500: '#00bcd4',
+          600: '#00acc1',
+          700: '#0097a7',
+          800: '#00838f',
+          900: '#006064',
+        },
+        blue: {
+          50: '#e3f2fd',
+          100: '#bbdefb',
+          200: '#90caf9',
+          300: '#64b5f6',
+          400: '#42a5f5',
+          500: '#2196f3',
+          600: '#1e88e5',
+          700: '#1976d2',
+          800: '#1565c0',
+          900: '#0d47a1',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
